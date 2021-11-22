ce5b10718130fe530da2e880d55bb96d1c0cc9af
Hannes Rüger <hannesrueger@gmx.de>
2021-11-22 15:14:02 +0100

    use proxy instead of api url magic
    [skip ci]

--- adventskalender/src/app/_services/remote.service.ts
+++ adventskalender/src/app/_services/remote.service.ts
@@ -3,34 +3,28 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, Subject } from "rxjs";
 import { StorageService } from "./storage.service";
 import { AuthenticationService } from "./authentication.service";
-import { getApiUrl } from "../_helpers/apiUrl";
 
 @Injectable({
     providedIn: "root",
 })
 export class RemoteService {
-    private pApiUrl = "";
     constructor(private httpClient: HttpClient, private storageService: StorageService) {
-        this.pApiUrl = getApiUrl();
     }
 
     public getImageUrl(url: string, authService: AuthenticationService): string {
-        return `${this.pApiUrl}/${url}?authorization=${authService.currentUser?.jwtToken}`;
+        return `/api/${url}?authorization=${authService.currentUser?.jwtToken}`;
     }
 
     public get(url: string): Observable<any> {
-        if (!this.pApiUrl) {
-            return new Subject();
-        }
-        return this.httpClient.get(`${this.pApiUrl}/${url}`);
+        return this.httpClient.get(`/api/${url}`);
     }
 
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     public post(url: string, data: { [key: string]: any }, options?: any): Observable<any> {
-        return this.httpClient.post(`${this.pApiUrl}/${url}`, data, options);
+        return this.httpClient.post(`/api/${url}`, data, options);
     }
 
     public delete(url: string): Observable<any> {
-        return this.httpClient.delete(`${this.pApiUrl}/${url}`);
+        return this.httpClient.delete(`/api/${url}`);
     }
 }
