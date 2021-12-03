8896f239ca43f884579b5b22a70d38cc40200c28
Hannes Rüger <hannesrueger@gmx.de>
2021-12-03 18:20:06 +0100

    fix build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,15 @@
+echo "--- Building frontend ---"
 cd ../adventskalender
 npm install
 npm run build -- --outputPath=../docker/frontend
+echo "--- Building backend ---"
 cd ../api
 npm install
-npx @zeit/ncc build src/index.ts -o ../docker/backend
+npx @vercel/ncc build src/index.ts -o ../docker/backend
+echo "--- Copying env ---"
 cd ..
 cp ./container-env.json ./docker/container-env.json
+echo "--- Building Container Image ---"
 cd ./docker
 PACKAGE_VERSION=$(cat ../adventskalender/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/adventskalender:v$PACKAGE_VERSION .
